0b2fd01e1d36dec17e58786fda7b3901f8471a46
Deepak <deepakpar64@gmail.com>
2024-08-30 10:11:04 +0530

    creat function to make addToCart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -57,13 +57,35 @@ products.forEach((product) => {
 
 })
 
+
 document.querySelector('.js-products-grid').
 innerHTML = productsHTML;
+
+function addToCart() {
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+        if (productId === item.productId) {
+            matchingItem = item;
+        }
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    }
+
+}
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
-
+        addToCart
 
 
         let matchingItem;
